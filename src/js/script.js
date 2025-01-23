e2360abe6c6f9cc372bc2b78b227c37d90aebcb4
kanako <kanako.inamine.96@gmail.com>
2025-01-23 13:19:13 +0100

    add_linkArea

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,3 @@
-
-// function setup() {
-//   createCanvas(500, 500, WEBGL);
-// }
-
-
-// import * as THREE from "three";
-
-// const width = 960;
-// const height = 540;
-
-// // レンダラーを作成
-// const renderer = new THREE.WebGLRenderer({
-//   canvas: document.querySelector('#myCanvas')
-// });
-// renderer.setSize(width, height);
-// renderer.setPixelRatio(devicePixelRatio);
-
-// // シーンを作成
-// const scene = new THREE.Scene();
-
-// // カメラを作成
-// const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-// // カメラの初期座標を設定（X座標:0, Y座標:0, Z座標:0）
-// camera.position.set(0, 0, 1000);
-
-// // 箱を作成
-// const geometry = new THREE.BoxGeometry(500, 500, 500);
-// const material = new THREE.MeshStandardMaterial({color: 0x0000FF});
-// const box = new THREE.Mesh(geometry, material);
-// scene.add(box);
-
-// // 平行光源
-// const light = new THREE.DirectionalLight(0xFFFFFF);
-// light.intensity = 2; // 光の強さを倍に
-// light.position.set(1, 1, 1); // ライトの方向
-// // シーンに追加
-// scene.add(light);
-
-// // 初回実行
-// tick();
-
-// function tick() {
-//   requestAnimationFrame(tick);
-
-//   // 箱を回転させる
-//   box.rotation.x += 0.01;
-//   box.rotation.y += 0.01;
-
-//   // レンダリング
-//   renderer.render(scene, camera);
-// }
-
-
 // Three.js の初期設定
 import * as THREE from 'three';
 
@@ -65,7 +11,7 @@ document.body.appendChild(renderer.domElement);
 // 原子核を3D円形に配置
 const nucleus = new THREE.Group();
 const nucleusMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const nucleusParticleCount = 10; // 粒子の数
+const nucleusParticleCount = 1; // 粒子の数
 const nucleusRadius = 1.5; // 円の半径
 
 for (let i = 0; i < nucleusParticleCount; i++) {
@@ -136,7 +82,7 @@ function animate() {
   requestAnimationFrame(animate);
 
   // 原子核の回転
-  nucleus.rotation.y += 0.01;
+  // nucleus.rotation.y += 0.01;
 
   // 電子軌道1の回転
   orbitGroup1.rotation.z += 0.01;
@@ -150,3 +96,4 @@ function animate() {
 }
 
 animate();
+
